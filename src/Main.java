3f08add558cb264b7aba3a2afd0db716550a4a68
chiky340 <halleychiky@gmail.com>
2025-02-06 23:11:14 +0200

    instantiated the scanner class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
         Game game = new Game();
+        int input = scanner.nextInt();
+        game.play(input);
 
     }
 }
