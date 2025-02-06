da45b590d20dc3720f32e537283f4bc829db5e45
chiky340 <halleychiky@gmail.com>
2025-02-06 23:11:46 +0200

    instantiated the scanner class

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
         Game game = new Game();
+        
+        System.out.print("enter an input: ");
         int input = scanner.nextInt();
         game.play(input);
 
