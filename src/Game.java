0baac1db809269c9b3c6cfe7017207c59e8e9f1c
chiky340 <halleychiky@gmail.com>
2025-02-06 23:18:44 +0200

    made the first condition

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,12 @@ public class Game {
     public void play(int input) {
         int random =  (int)Math.random()*10;
 
+        while(true){
+            if (input==random){
+                System.out.println("you guessed right");
+            }
+
+        }
 
     }
 }
