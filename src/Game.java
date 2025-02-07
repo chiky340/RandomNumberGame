80963f52f4f481995053410d58f44e9251e38e37
chiky340 <halleychiky@gmail.com>
2025-02-07 20:57:15 +0200

    finished game and cheat

--- src/Game.java
+++ src/Game.java
@@ -7,7 +7,7 @@ public class Game {
 
     private int random;
     public void play() {
-        random =  (int)(Math.random()*10);
+        random =  (int)(Math.random()*100);
 
         while(true){
             var scanner = new Scanner(System.in);
@@ -17,15 +17,14 @@ public class Game {
             if (input==random){
                 System.out.println("you guessed right");
                 break;
-            }
-            else if (input>random){
+            } else if (input==1907) {
+                this.Cheat();
+                continue;
+            } else if (input>random){
                 System.out.println("too high");
                 continue;
             } else if (input<random) {
                 System.out.println("too low");
-                continue;
-            } else if (input==1907) {
-                this.Cheat();
                 continue;
             }
         }
