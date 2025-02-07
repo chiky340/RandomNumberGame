a5d9f1f11371e4ee6dfae2ee8d12c348e1c75da2
chiky340 <halleychiky@gmail.com>
2025-02-07 20:28:00 +0200

    completed the game

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
         Game game = new Game();
-        
-        System.out.print("enter an input: ");
-        int input = scanner.nextInt();
-        game.play(input);
+        game.play();
 
     }
 }
